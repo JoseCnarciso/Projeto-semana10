becf120d231807695410cfa7c2f0ea9dccef63ca
JoseCnarciso <josecdia@hotmail.com>
2023-08-07 19:39:45 -0300

    iniciando projeto

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+    Oi boizão <br/>
+
+    {{ message }}
+</template>
+
+<script>
+    export default{
+        data(){
+            return{
+                message: 'Oi boizinho'
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
